17b6ca78b1108c895ffed987a2ee820ae117e9fc
M0onny <158460133+M0onny@users.noreply.github.com>
2024-02-01 14:38:49 +0300

    01.02.2024 Задачи 1-5

---
+++ main (4).py
@@ -0,0 +1,36 @@
+#coding: utf-8
+#задача 1 
+x = int(input("Введите x = "))
+y = 7*x+5
+print("Ответ y =", y)
+
+
+#задача 2 
+a = int(input("Введите a = "))
+P = a*a*a*a
+print("Ответ P =", P)
+
+
+#задача 3
+r = int(input("Введите r = "))
+Пи = 3.14
+C = 2*Пи*r
+print("Ответ C =", C)
+
+
+#задача 4 
+d = int(input("Введите d = "))
+Пи = 3.14
+S = Пи*d*d/4
+print("Ответ S =", S)
+
+
+#задача 5
+a = int(input("Введите a = "))
+b = int(input("Введите b = "))
+c = a+b
+a1 = a 
+b1 = b
+c1 = a1+b1
+c2 = c*c1*0.5
+print("Ответ c =", c2)
